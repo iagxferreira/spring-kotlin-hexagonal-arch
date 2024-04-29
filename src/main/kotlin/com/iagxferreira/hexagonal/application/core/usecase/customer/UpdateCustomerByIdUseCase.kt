1ba0285724c4962b197eaec8c09443c8c22f384b
Iago Ferreira <iago-ferreira@outlook.com>
2024-04-29 02:22:28 -0300

    feat: add Customer find and update usecases

--- src/main/kotlin/com/iagxferreira/hexagonal/application/core/usecase/customer/UpdateCustomerByIdUseCase.kt
+++ src/main/kotlin/com/iagxferreira/hexagonal/application/core/usecase/customer/UpdateCustomerByIdUseCase.kt
@@ -1,14 +1,16 @@
 package com.iagxferreira.hexagonal.application.core.usecase.customer
 
 import com.iagxferreira.hexagonal.application.core.domain.Customer
+import com.iagxferreira.hexagonal.application.ports.`in`.FindCustomerByIdInputPort
 import com.iagxferreira.hexagonal.application.ports.`in`.UpdateCustomerInputPort
-import com.iagxferreira.hexagonal.application.ports.out.InsertCustomerOutputPort
 import com.iagxferreira.hexagonal.application.ports.out.UpdateCustomerOutputPort
 
 class UpdateCustomerByIdUseCase(
+    private val findCustomerByIdInputPort: FindCustomerByIdInputPort,
     private val updateCustomerOutputPort: UpdateCustomerOutputPort
 ): UpdateCustomerInputPort {
     override fun update(id: String, customer: Customer): Customer? {
+        val foundedCustomer = findCustomerByIdInputPort.find(id) ?: throw RuntimeException("Customer not found")
         return updateCustomerOutputPort.update(id, customer)
     }
 }
